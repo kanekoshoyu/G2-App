b01631dd7980886dbd201428186f5ec557a05fa0
kanekoshoyu <kanekoshoyu@gmail.com>
2018-03-28 17:30:17 +0800

    UI

--- app/src/main/java/hk/hku/idp/MainActivity.java
+++ app/src/main/java/hk/hku/idp/MainActivity.java
@@ -2,6 +2,8 @@ package hk.hku.idp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +12,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void goBasic(View v) {
+        setContentView(R.layout.activity_basicbutton);
+    }
+
+    protected void onButtonPressed(View v) {
+        String text_on_button = ((Button) findViewById(R.id.button_basic)).getText().toString();
+        if (text_on_button.equals("ON")){
+            text_on_button="OFF";
+        }else{
+            text_on_button="ON";
+        }
+        ((Button) findViewById(R.id.button_basic)).setText(text_on_button);
+    }
+
 }
